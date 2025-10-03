205af418fe37137d316bf30b230d613417194d73
Aaryanpatidar <aaryanpatidar1211@gmail.com>
2025-10-03 10:28:43 +0530

    Adding code and screenshot

--- EmployyeDBApp/src/DBConnection.java
+++ EmployyeDBApp/src/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/companydb";
     private static final String USER = "root";
-    private static final String PASSWORD = "Aaryan@1230";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         try {
